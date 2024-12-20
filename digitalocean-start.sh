d1143f8525884b374bcaa1b6351e4c66ad25135f
oreoro <bilalarshad@192.168.1.4>
2024-12-20 23:54:18 +0500

    sh

--- digitalocean-start.sh
+++ digitalocean-start.sh
@@ -1,7 +1,12 @@
 
+#!/bin/sh
+
+# Set the VITE_FRONTEND_URL environment variable with a default value
 export VITE_FRONTEND_URL=${APP_FRONTEND_URL:-"/"}
 
+# Print the environment variable for debugging purposes
 echo "Starting with the following environment variables:"
 echo "VITE_FRONTEND_URL=${VITE_FRONTEND_URL}"
 
+# Execute the startup script
 exec /startup.sh
